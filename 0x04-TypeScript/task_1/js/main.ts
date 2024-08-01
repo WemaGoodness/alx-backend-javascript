79cda58506a6e908a9c6f4fcaafdfc6e91eb10a3
WemaGoodness <goodnesswema20@gmail.com>
2024-08-01 03:28:56 +0300

    TypeScript Task1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,43 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const teacher1: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher1);
+
+const teacher2: Teacher = {
+  firstName: 'Jane',
+  lastName: 'Smith',
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: 'New York',
+  contract: true,
+};
+
+console.log(teacher2);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
